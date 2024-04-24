9eeb9410a4586da0f3d2ab70cc7174cd69dfb56b
Konstantin Onishchenko <kostik80_80@mail.ru>
2024-04-24 17:57:04 +0300

    Поправил json в utf-8 и поменял переменные для базы данных

--- config/settings.py
+++ config/settings.py
@@ -81,11 +81,11 @@ WSGI_APPLICATION = 'config.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': os.getenv('NAME_DATABASE'),
-        'USER': os.getenv('USER_DATABASE'),
-        'PASSWORD': os.getenv('PASSWORD_DATABASE'),
-        'HOST': os.getenv('LOCAL_HOST'),
-        'PORT': os.getenv('PORT'),
+        'NAME': os.getenv('POSTGRES_DB'),
+        'USER': os.getenv('POSTGRES_USER'),
+        'PASSWORD': os.getenv('POSTGRES_PASSWORD'),
+        'HOST': os.getenv('POSTGRES_HOST'),
+        'PORT': os.getenv('POSTGRES_PORT'),
 
     }
 }
