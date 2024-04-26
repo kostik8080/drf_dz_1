f2d367e26309d1bb7477f8693a4b22bf95704de3
Konstantin Onishchenko <kostik80_80@mail.ru>
2024-04-26 22:13:00 +0300

    Добавление разрешений для модератора без группы

--- courses/permissions.py
+++ courses/permissions.py
@@ -4,7 +4,7 @@ from rest_framework.permissions import BasePermission, SAFE_METHODS
 class IsModer(BasePermission):
     """Проверяет, что пользователь является модератором."""
     def has_permission(self, request, view):
-        if request.user.is_staff and request.user.groups.filter(name="moderator").exists():
+        if request.user.groups.filter(name="moderator").exists():
             return True
         return False
 
